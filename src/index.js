10348ab1233d9e5b357c64b77a0a7848811d5aeb
Paul Sealock <psealock@gmail.com>
2020-09-24 14:10:17 +1200

    name passed

--- src/index.js
+++ src/index.js
@@ -4,24 +4,38 @@ const { WooNavigationMenu, WooNavigationItem } = window.wc;
 
 const MyPlugin = () => {
   return (
-    <>
-      <WooNavigationMenu menu="woocommerce">
-        <WooNavigationItem
-          item="example-1"
-          title="Examples"
-          navigateToMenu="my-plugin-examples"
-        />
-      </WooNavigationMenu>
-      <WooNavigationMenu menu="my-plugin-examples" parentMenu="woocommerce">
-        <WooNavigationItem item="example-1">
-          <a href="www.example.com/1">Example 1</a>
-        </WooNavigationItem>
-        <WooNavigationItem item="example-2">
-          <a href="www.example.com/2">Example 2</a>
-        </WooNavigationItem>
-      </WooNavigationMenu>
-    </>
+    <WooNavigationMenu menu="woocommerce">
+      <WooNavigationItem
+        item="example-1"
+        title="Examples"
+        navigateToMenu="my-plugin-examples"
+      />
+    </WooNavigationMenu>
   );
 };
 
 registerPlugin("my-plugin", { render: MyPlugin });
+
+// return (
+//   <>
+//     <WooNavigationMenu menu="woocommerce">
+//       <WooNavigationItem
+//         item="example-1"
+//         title="Examples"
+//         navigateToMenu="my-plugin-examples"
+//       />
+//     </WooNavigationMenu>
+//     <WooNavigationMenu menu="my-plugin-examples" parentMenu="woocommerce">
+//       <WooNavigationItem item="example-1">
+//         <a className="components-button" href="www.example.com/1">
+//           Example 1
+//         </a>
+//       </WooNavigationItem>
+//       <WooNavigationItem item="example-2">
+//         <a className="components-button" href="www.example.com/2">
+//           Example 2
+//         </a>
+//       </WooNavigationItem>
+//     </WooNavigationMenu>
+//   </>
+// );
