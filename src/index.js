aed412ca49d59dd62a05fa09446e09df5f1d77b8
Paul Sealock <psealock@gmail.com>
2020-09-24 15:31:44 +1200

    save

--- src/index.js
+++ src/index.js
@@ -1,40 +1,34 @@
 import { registerPlugin } from "@wordpress/plugins";
 
-const { WooNavigationMenu, WooNavigationItem } = window.wc;
+const {
+  WooNavigationMenu,
+  WooNavigationItem,
+  RawItem: NavigationItem,
+} = window.wc;
 
 const MyPlugin = () => {
   return (
-    <WooNavigationItem
-      menu="woocommerce"
-      item="example-1"
-      title="Examples"
-      navigateToMenu="my-plugin-examples"
-    />
+    <>
+      <WooNavigationItem
+        menu="woocommerce"
+        item="example-1"
+        title="Examples"
+        navigateToMenu="my-plugin-examples"
+      />
+      <WooNavigationMenu menu="my-plugin-examples" parentMenu="woocommerce">
+        <NavigationItem item="example-1">
+          <a className="components-button" href="www.example.com/1">
+            Example 1
+          </a>
+        </NavigationItem>
+        <NavigationItem item="example-2">
+          <a className="components-button" href="www.example.com/2">
+            Example 2
+          </a>
+        </NavigationItem>
+      </WooNavigationMenu>
+    </>
   );
 };
 
 registerPlugin("my-plugin", { render: MyPlugin });
-
-// return (
-//   <>
-//     <WooNavigationMenu menu="woocommerce">
-//       <WooNavigationItem
-// item="example-1"
-// title="Examples"
-// navigateToMenu="my-plugin-examples"
-//       />
-//     </WooNavigationMenu>
-//     <WooNavigationMenu menu="my-plugin-examples" parentMenu="woocommerce">
-//       <WooNavigationItem item="example-1">
-//         <a className="components-button" href="www.example.com/1">
-//           Example 1
-//         </a>
-//       </WooNavigationItem>
-//       <WooNavigationItem item="example-2">
-//         <a className="components-button" href="www.example.com/2">
-//           Example 2
-//         </a>
-//       </WooNavigationItem>
-//     </WooNavigationMenu>
-//   </>
-// );
