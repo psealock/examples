3721adcb49d945aed43d6e3b1fbe881e7cb95cff
Paul Sealock <psealock@gmail.com>
2020-11-24 15:51:58 +1300

    temporary fix

--- examples.php
+++ examples.php
@@ -23,8 +23,7 @@ function add_extension_register_script() {
 	wp_register_script(
 		'examples',
 		$script_url,
-		// array_merge( array( WC_ADMIN_APP ), $script_asset['dependencies'] ),
-		$script_asset['dependencies'],
+		array_merge( array( 'wp-element', 'wp-data', 'moment' ), $script_asset['dependencies'] ),
 		$script_asset['version'],
 		true
 	);
